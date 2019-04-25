d869c8ed592b6340f1ded83926c0b0e1d4815ed4
tang <eytang8@gmail.com>
2019-04-24 17:35:04 -0700

    basic multipage functionality

---
+++ screens/CharSelectScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default (props)=>{
+  return(
+  <View>
+    <Text>
+      Hello World
+    </Text>
+  </View>
+  )
+}
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  Button,
   Image,
   Platform,
   ScrollView,
@@ -7,27 +8,12 @@ import {
   Text,
   TouchableOpacity,
   View,
+  Alert
 } from 'react-native';
 import { WebBrowser } from 'expo';
 
 import { MonoText } from '../components/StyledText';
 import CharSelect from '../components/CharSelect';
-/*
-{
-  name: '',
-  charClass: '',
-  desc: '',
-  traits: {
-    charm: 0,
-    cool: 0,
-    sharp: 0,
-    tough: 0,
-    weird: 0
-    },
-  moves: ['punch', 'kick']
-}
-
-*/
 
 function CharConstructor(name, charClass, desc, traits, moves){
   this.name = name;
@@ -57,100 +43,11 @@ export default class HomeScreen extends React.Component {
     const char2 = new CharConstructor('alice', 'normie', 'totally unremarkable', traits2, ['punch', 'kick', 'talk', 'joke']);
     chars.push(char1);
     chars.push(char2);
+
     return (
-      <View style={styles.container}>
-      <Text>{JSON.stringify(chars)}</Text>
+      <View style={{flex:1,flexDirection: 'column'}}>
         <CharSelect characters={chars}/>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
-});
