00374aff477693688dd1bffc4ea598ad12b1c549
Shane Taylor <shaneallantaylor@gmail.com>
2019-04-24 20:17:26 -0700

    basic changes to figure things out

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -12,7 +12,7 @@ import { WebBrowser } from 'expo';
 
 import { MonoText } from '../components/StyledText';
 import CharSelect from '../components/CharSelect';
-import LoginScreen from './LoginScreen';
+// import LoginScreen from './LoginScreen';
 /*
 {
   name: '',
@@ -30,14 +30,14 @@ import LoginScreen from './LoginScreen';
 
 */
 
-function CharConstructor(name, charClass, desc, traits, moves){
+function CharConstructor(name, charClass, desc, traits, moves) {
   this.name = name;
   this.charClass = charClass;
   this.desc = desc;
   this.traits = traits;
   this.moves = moves;
 }
-function TraitsConstructor(charm, cool, sharp, tough, weird){
+function TraitsConstructor(charm, cool, sharp, tough, weird) {
   this.charm = charm;
   this.cool = cool;
   this.sharp = sharp;
@@ -47,23 +47,24 @@ function TraitsConstructor(charm, cool, sharp, tough, weird){
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
-    header: null,
+    title: 'Welcome baby!'
   };
 
   render() {
     const chars = [];
-    const traits1 = new TraitsConstructor(1,2,3,4,5);
-    const traits2 = new TraitsConstructor(2,1,4,3,5);
+    const traits1 = new TraitsConstructor(1, 2, 3, 4, 5);
+    const traits2 = new TraitsConstructor(2, 1, 4, 3, 5);
     const char1 = new CharConstructor('shane', 'druid', 'an honest man', traits1, ['punch', 'kick', 'talk', 'joke']);
     const char2 = new CharConstructor('alice', 'normie', 'totally unremarkable', traits2, ['punch', 'kick', 'talk', 'joke']);
     chars.push(char1);
     chars.push(char2);
     return (
-      // <View style={styles.container}>
-      <LoginScreen />
-      // {/* <Text>{JSON.stringify(chars)}</Text> */}
-        // {/* <CharSelect characters={chars}/> */}
-      // </View>
+      <View style={styles.container}>
+
+        {/* // <LoginScreen /> */}
+        <Text>{JSON.stringify(chars)}</Text>
+        <CharSelect characters={chars} />
+      </View>
     );
   }
 }
