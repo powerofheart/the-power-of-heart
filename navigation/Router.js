7aa7dc43c8f3a7071d04a8e2adf1479d19d31d1a
tang <eytang8@gmail.com>
2019-04-25 15:02:45 -0700

    working on nav

--- navigation/Router.js
+++ navigation/Router.js
@@ -1,15 +1,20 @@
+import React from 'react';
+import {Text, View} from 'react-native';
 import { createStackNavigator, createAppContainer } from "react-navigation";
 
-import LoginScreen from '../screens/LoginScreen';
+import LoginComp from '../components/LoginComp';
 import CharSelectScreen from '../screens/CharSelectScreen'
 
-const LoginNav = createStackNavigator({
-    Login: LoginScreen,
+const LoginNav = createStackNavigator(
+  {
+    Login: LoginComp,
     CharSelect: CharSelectScreen
   },
   {
     initialRouteName: "Login"
   }
-})
+)
 
-export const LoginContainer = createAppContainer(LoginNav)
+const LoginContainer = createAppContainer(LoginNav)
+
+export default LoginContainer;
