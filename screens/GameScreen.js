3fd272f2ab77baeeea73d0bc3da16e61506f232c
justnoah <noahxman@gmail.com>
2019-04-25 17:34:07 -0700

    Fleshed out game screen; GameScreen.js.

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -2,29 +2,50 @@
 import React from 'react';
 import {
   Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
   Text,
-  TouchableOpacity,
   View,
 } from 'react-native';
-import { WebBrowser } from 'expo';
-
-import { MonoText } from '../components/StyledText';
-import CharSelect from '../components/CharSelect';
 
 import styles from '../styles/gameScreen.scss';
 // import styles from '../styles/_colors.scss';
 
 export default class GameScreen extends React.Component {
-
-    render(){
-        return (
-            <View>
-                <Text style={styles.test}>Play</Text>
-            </View>
-        )
-
+  constructor() {
+    super()
+    this.state = {
+      moveList: [ 'kick', 'punch', 'cry' ]
     }
+  }
+
+  render(){
+    const moveListArray = this.state.moveList.map((item, i) => {
+      <Text key={ i }>
+        { item }
+      </Text> 
+    })
+    return (
+      <View>
+        <Image />
+        <View>
+          <Text style={styles.test}>Name  |  { charClass }</Text>
+          <Text>Harm: { harmCount }</Text>
+          <Text>Luck: { luckCount }</Text> 
+        </View>
+      </View>
+      <Text>
+        Charm: { charmCount }  
+        |  Cool: { coolCount }  
+        |  Sharp: { sharpCount }  
+        |  Tough: { toughCount }  
+        |  Weird: { weirdCount }
+      </Text>
+      <Text>Character Move List</Text>
+      { moveListArray }
+      <View>
+        <Button>Back</Button>
+        <Button>Roll</Button>
+      </View>
+    )
+
+  }
 }
